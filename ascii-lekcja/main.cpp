14558874dadb73937bc273b0a9cd45e0d3c4196e
Pomarancz789 <granat-02krawcy@icloud.com>
2022-02-23 09:46:35 +0100

    to nie działa XD

--- ascii-lekcja/main.cpp
+++ ascii-lekcja/main.cpp
@@ -1,14 +1,22 @@
-//
-//  main.cpp
-//  ascii-lekcja
-//
-//  Created by Jakub Wtorkiewicz on 23/02/2022.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    string binarna="101";
+    
+    int suma=0;
+    int dlugosc=binarna.size();
+    
+    for (int i=dlugosc-1; i>=0; i--)
+    {
+        suma= suma + ((binarna[i]-(int)'0')*2^(dlugosc-i));
+        
+        
+    }
+    
+    cout << suma << endl;
+    
+    
     return 0;
 }
